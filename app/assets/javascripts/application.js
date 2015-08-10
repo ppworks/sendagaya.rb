0b774830aef17702878666f909fec1885d291db7
koshikawa <koshikawa@ppworks.jp>
2015-08-10 20:55:31 +0900

    top sample

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+
+window.onload = function() {
+  require('./top.es6');
+};
---
+++ app/assets/javascripts/top.es6
@@ -0,0 +1,5 @@
+import React from 'react'
+React.render(
+  <div>hoge</div>,
+  document.getElementById('target')
+)
